c9eb3213dbd0d99e73a71476f9c7e15e1a351bd2
Dr123Ake <50765120+Dr123Ake@users.noreply.github.com>
2023-10-20 14:57:13 +0800

    Update simple-interest.sh Task7

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Dr123Ake
    # Input:
    # p, principal amount
    # t, time period in years
